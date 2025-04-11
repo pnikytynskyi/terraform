e13a9bc01d60f35910cccf41ecea12f84be7e43d
pavlonikytysnkyi <pnikytynskyi@gmail.com>
2025-04-11 21:19:00 +0200

    04

--- 04-backends-s3/providers.tf
+++ 04-backends-s3/providers.tf
@@ -11,6 +11,11 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-pavlo.nikytynskyi-bucket"
+    key    = "04-backends/state.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
